bfefb74c6b26d9c0b47fd7be646b49e6410dee63
hiamtrung103 <141995711+hiamtrung103@users.noreply.github.com>
2023-08-12 23:52:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,64 @@
-require('dotenv').config();
-const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
-const client = new Client({ intents: [
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.MessageContent,
-]})
+const { Client, Events, GatewayIntentBits } = require("discord.js")
+require("dotenv/config")
+const { OpenAIApi, Configuration } = require("openai")
 
-
-const { Configuration, OpenAIApi } = require('openai');
-const configuration = new Configuration({
-      organization: process.env.OPENAI_ORG,
+const config = new Configuration({
       apiKey: process.env.OPENAI_KEY
-});
-const openai = new OpenAIApi(configuration);
-
-client.on('messageCreate', async function(message){
-      try {
-            // console.log(message);
-            if(message.author.bot) return;
-            if(message.channel.id !== '1072935576854073354' && message.channel.id !== '1072947276240134226') return;
-            if(message.content.startsWith('!')) return;
-
-            let conversationLog = [{ role: 'system', content: "Bạn là người bạn tốt." }];
-
-            await message.channel.sendTyping();
-
-            let prevMessages = await message.channel.messages.fetch({ limit: 15 });
-            prevMessages.reverse();
-
-            prevMessages.forEach((msg) =>{
-                  if(message.content.startsWith('!')) return;
-                  if(msg.author.id !== client.user.id && message.author.bot) return;
-                  if(msg.author.id !== message.author.id) return;
-
-                  conversationLog.push({
-                        role: 'user',
-                        content: msg.content,
-                  })
-            })
-
-            const result = await openai.createChatCompletion({
-                  model: 'gpt-3.5-turbo',
-                  messages: conversationLog,
-            })
-
-            message.reply(result.data.choices[0].message);
-      } catch (error) {
-            console.log(err)
-      }
 })
 
-client.login(process.env.TOKEN).then(() => {
-      console.log("ChatBot đã sẵn sàng trên discord");
-      client.user.setActivity({
-            name: "Trung",
-            type: ActivityType.Streaming,
-            url: "https://www.youtube.com/watch?v=nInxHXD5mt4"
+const openai = new OpenAIApi(config)
+
+const client = new Client({
+      intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent
+      ]
+})
+
+client.once(Events.ClientReady, (clientUser) => {
+      console.log(`Đăng nhập thành công ${clientUser.user.tag}`)
+})
+
+client.login(process.env.BOT_TOKEN)
+
+const BOT_CHANNEL= "123" //Nhập ID channel riêng cho ChatBot
+const PAST_MESSAGES = 5
+
+client.on(Events.MessageCreate, async (message) => {
+      if (message.author.bot) return
+      if (message.channel.id !== BOT_CHANNEL) return
+
+      message.channel.sendTyping()
+
+      let messages = Array.from(await message.channel.messages.fetch({
+            limit: PAST_MESSAGES,
+            before: message.id
+      }))
+      messages = messages.map(m=>m[1])
+      messages.unshift(message)
+
+      let users = [...new Set([...messages.map(m=> m.member.displayName), client.user.username])]
+
+      let lastUser = users.pop()
+
+      let prompt = `Đây là cuộc trò chuyện bí mật giữa chúng ta ${users.join(", ")}, và ${lastUser}. \n\n`
+
+      for (let i = messages.length - 1; i >= 0; i--) {
+            const m = messages[i]
+            prompt += `${m.member.displayName}: ${m.content}\n`
+      }
+      prompt += `${client.user.username}:`
+      console.log("Nhắc nhở:", prompt)
+
+      const response = await openai.createCompletion({
+            prompt,
+            model: "text-davinci-003",
+            max_tokens: 1000, // Số chữ cho phép tối đa của Chatbot, Khuyến cáo là nên để 500~1000 nhé
+            stop: ["\n"]
       })
-});
+
+      console.log("Trả lời", response.data.choices[0].text)
+      await message.channel.send(response.data.choices[0].text)
+
+})
